177a9854e473a92f0dd12d8cfe3269c02eae59e0
Jin Park <jinpark1504@gmail.com>
2024-07-15 11:51:56 +0900

    Add greeting and student to routes

--- helloWorld/Sources/App/routes.swift
+++ helloWorld/Sources/App/routes.swift
@@ -8,4 +8,32 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("greeting", ":name") { req async -> String in
+        guard let guest = req.parameters.get("name") else {
+            return "Hi!"
+        }
+        
+        return "Hi, \(guest), greetings! Thanks for visiting us."
+    }
+    
+    app.get("student", ":name") { req async throws -> String in
+        guard let studentName = req.parameters.get("name") else {
+            throw Abort(.badRequest)
+        }
+        
+        let studentRecords = [
+            "Peter": 3.42,
+            "Thomas": 2.98,
+            "Jane": 3.91,
+            "Ryan": 4.00,
+            "Kyle": 4.00,
+        ]
+        
+        if let gpa = studentRecords[studentName] {
+            return "The student \(studentName)'s GPA is \(gpa)"
+        } else {
+            return "The student's record cna't be found"
+        }
+    }
 }
